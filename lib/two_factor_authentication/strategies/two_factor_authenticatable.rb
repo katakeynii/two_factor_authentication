6a78e112fae2a738d965398e20c446dfc448d3ff
Mohamed Camara GUEYE <mohamed.gueye@myriadgroup.com>
2021-07-28 20:59:51 +0000

    adding strategies

--- lib/two_factor_authentication/strategies/two_factor_authenticatable.rb
+++ lib/two_factor_authentication/strategies/two_factor_authenticatable.rb
@@ -4,15 +4,7 @@ module Devise
         class TwoFactorAuthenticatable < Devise::Strategies::Base
             def authenticate!
                 resource  = mapping.to.find_for_database_authentication(authentication_hash)
-                hashed = false
-
-                if validate(resource){ hashed = true; resource.valid_password?(password) }
-                    super
-                end
-
-                fail(Devise.paranoid ? :invalid : :not_found_in_database) unless resource
-
-                @halted = false if @result == :failure
+                success!(resource)
             end
         end
     end
