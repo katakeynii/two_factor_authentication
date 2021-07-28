21f1d70db999946fb117cb27aea6fc15112f559b
Mohamed Camara GUEYE <mohamed.gueye@myriadgroup.com>
2021-07-28 20:43:36 +0000

    adding strategies

--- lib/two_factor_authentication/strategies/two_factor_authenticatable.rb
+++ lib/two_factor_authentication/strategies/two_factor_authenticatable.rb
@@ -3,23 +3,17 @@ module Devise
     #   class TwoFactorAuthenticatable < Devise::Strategies::DatabaseAuthenticatable
         class TwoFactorAuthenticatable < Devise::Strategies::Base
             def authenticate!
-                resource  = password.present? && mapping.to.find_for_database_authentication(authentication_hash)
+                resource  = mapping.to.find_for_database_authentication(authentication_hash)
                 hashed = false
 
                 if validate(resource){ hashed = true; resource.valid_password?(password) }
-                  remember_me(resource)
-                  resource.after_database_authentication
-                  success!(resource)
+                    super
                 end
 
-                # In paranoid mode, hash the password even when a resource doesn't exist for the given authentication key.
-                # This is necessary to prevent enumeration attacks - e.g. the request is faster when a resource doesn't
-                # exist in the database if the password hashing algorithm is not called.
-                mapping.to.new.password = password if !hashed && Devise.paranoid
-                unless resource
-                  Devise.paranoid ? fail(:invalid) : fail(:not_found_in_database)
-                end
-              end
+                fail(Devise.paranoid ? :invalid : :not_found_in_database) unless resource
+
+                @halted = false if @result == :failure
+            end
         end
     end
   end
